817add1f2294624a65e978c2696e075c58b1b81a
Shahab <msb@technologies.dk>
2025-04-22 10:08:32 +0500

    reading txt file and print its lines

--- solution.js
+++ solution.js
@@ -1,1 +1,12 @@
-console.log("Testing");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+rl.on("line", function (line) {
+  if (line.trim() === "") return;
+  console.log(line);
+});
